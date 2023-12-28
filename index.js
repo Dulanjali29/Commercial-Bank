ae9dc75213d5754c56b597de9b0a5168880c32b2
Dulanjali29 <dulanjiariyarathna@gmail.com>
2023-12-28 14:55:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const mouseClick=()=>{
+    var icon=document.getElementById('float-menu');
+    if(icon.style.display=='none'){
+        icon.style.display='block';
+    }else{
+        icon.style.display='none';
+    }
+}
