bde2748420761f384817a815ba9551da3a9b03f6
Dulanjali29 <dulanjiariyarathna@gmail.com>
2023-12-29 20:17:09 +0530

    update-mouse-event-section

--- index.js
+++ index.js
@@ -16,20 +16,54 @@ const mouseOut=()=>{
 
 $(document).ready(function(){
    
-    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking').hide();
-})
-$("#personal_bank").click(function(){
-    $('.nav_business_bank,.nav_non_resident_banking').hide();
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking,.nav_services').hide();
+
+$("#personal_bank").mouseover(function(){
+    $("#personal_bank").css("color","#4972cf");
+    $('.nav_business_bank,.nav_non_resident_banking,.nav_services').hide();
     $('.nav_personal_bank').show();
+
+
   
 })
-$("#business_bank").click(function(){
+$("#business_bank").mouseover(function(){
+    $("#business_bank").css("color","#4972cf");
     $('.nav_business_bank').show();
-    $('.nav_personal_bank,.nav_non_resident_banking').hide();
+    $('.nav_personal_bank,.nav_non_resident_banking,.nav_services').hide();
   
 })
-$("#non_resident_banking").click(function(){
+$("#non_resident_banking").mouseover(function(){
+    $("#non_resident_banking").css("color","#4972cf");
     $('.nav_non_resident_banking').show();
-    $('.nav_personal_bank,.nav_business_bank,').hide();
+    $('.nav_personal_bank,.nav_business_bank,.nav_services').hide();
+  
+})
+$("#services").mouseover(function(){
+    $("#services").css("color","#4972cf");
+    $('.nav_services').show();
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking').hide();
+  
+})
+
+
+})
+$("#personal_bank").mouseout(function(){
+    $("#personal_bank").css("color"," black");
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking,.nav_services').hide(); 
+  
+})
+$("#business_bank").mouseout(function(){
+    $("#business_bank").css("color"," black");
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking,.nav_services').hide(); 
+  
+})
+$("#non_resident_banking").mouseout(function(){
+    $("#non_resident_banking").css("color"," black");
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking,.nav_services').hide(); 
+  
+})
+$("#services").mouseout(function(){
+    $("#services").css("color"," black");
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking,.nav_services').hide(); 
   
 })
