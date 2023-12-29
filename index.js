8605747d6438e97c39496ee1f05b37881b89a703
Dulanjali29 <dulanjiariyarathna@gmail.com>
2023-12-29 15:49:13 +0530

    mouse-event-section-nav_item1 complete

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ const mouseOver=()=>{
 const mouseOut=()=>{
     document.getElementById('hover-img').src='./assests/hover1.png'
 }
+
+
+$(document).ready(function(){
+   
+    $('.nav_personal_bank,.nav_business_bank',).hide();
+})
+$("#personal_bank").click(function(){
+    $('.nav_business_bank').hide();
+    $('.nav_personal_bank').show();
+  
+})
+$("#business_bank").click(function(){
+    $('.nav_business_bank').show();
+    $('.nav_personal_bank').hide();
+  
+})
