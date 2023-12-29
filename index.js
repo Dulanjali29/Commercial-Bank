07472e335597d90d5bc8faff794f9ca2b0b523e7
Dulanjali29 <dulanjiariyarathna@gmail.com>
2023-12-29 17:09:41 +0530

    mouse-event-section-nav_item3 finish

--- index.js
+++ index.js
@@ -16,15 +16,20 @@ const mouseOut=()=>{
 
 $(document).ready(function(){
    
-    $('.nav_personal_bank,.nav_business_bank',).hide();
+    $('.nav_personal_bank,.nav_business_bank,.nav_non_resident_banking').hide();
 })
 $("#personal_bank").click(function(){
-    $('.nav_business_bank').hide();
+    $('.nav_business_bank,.nav_non_resident_banking').hide();
     $('.nav_personal_bank').show();
   
 })
 $("#business_bank").click(function(){
     $('.nav_business_bank').show();
-    $('.nav_personal_bank').hide();
+    $('.nav_personal_bank,.nav_non_resident_banking').hide();
+  
+})
+$("#non_resident_banking").click(function(){
+    $('.nav_non_resident_banking').show();
+    $('.nav_personal_bank,.nav_business_bank,').hide();
   
 })
